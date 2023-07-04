d563b0201b12fab2dd4f7f5301c70e9b723c1775
Sabry Awad <dr.sabry1997@gmail.com>
2023-07-04 08:25:49 +0300

    🔧 refactor(store.rs): Refactor store implementation for adding subscribers
    The `Store` struct in the `store.rs` module has been refactored to add support for subscribers. Subscribers are functions that get called whenever the state in the store is updated.
    
    Changes made:
    - Added type aliases `Subscriber<T>` and `Subscribers<T>` to represent the subscriber function and a map of subscribers, respectively.
    - Added a `subscribers` field of type `Arc<RwLock<Subscribers<State>>>` to store the subscribers.
    - Added a `next_subscriber_id` field of type `Arc<AtomicUsize>` to generate unique subscriber IDs.
    - Modified the `thread::spawn` block to iterate over the subscribers and call them with the updated state whenever an action is received.
    - Implemented the `subscribe` method to add a subscriber and generate an unsubscribe closure.
    - Implemented the `unsubscribe` method to remove a subscriber from the map.
    
    The changes allow components to subscribe to state updates and execute custom logic when the state changes.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/store.rs
+++ src/store.rs
@@ -1,13 +1,24 @@
-use std::{sync::Arc, thread};
+use std::{
+    collections::HashMap,
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
+    thread,
+};
 
 use crossbeam_channel::{unbounded, Sender};
 use parking_lot::RwLock;
 
 use crate::reducer::Reducer;
 
+type Subscriber<T> = Box<dyn Fn(&T) + Send + Sync>;
+type Subscribers<T> = HashMap<usize, Subscriber<T>>;
 pub struct Store<State, Action> {
     state: Arc<RwLock<State>>,
     dispatcher: Sender<Action>,
+    subscribers: Arc<RwLock<Subscribers<State>>>,
+    next_subscriber_id: Arc<AtomicUsize>,
 }
 
 impl<State, Action> Store<State, Action>
@@ -28,18 +39,32 @@ where
         R: Reducer<State, Action> + Send + 'static,
     {
         let state = Arc::new(RwLock::new(initial_state));
+        let subscribers: Arc<RwLock<Subscribers<State>>> = Arc::new(RwLock::new(HashMap::new()));
+        let next_subscriber_id = Arc::new(AtomicUsize::new(0));
         let (dispatcher, receiver) = unbounded();
 
         let cloned_state = state.clone();
+        let cloned_subscribers = subscribers.clone();
+
         thread::spawn(move || {
             while let Ok(action) = receiver.recv() {
                 let state = &mut cloned_state.write();
                 let new_state = root_reducer.reduce(&**state, action);
                 **state = new_state;
+
+                let subscriber_map = cloned_subscribers.read();
+                for subscriber in subscriber_map.values() {
+                    subscriber(&*state);
+                }
             }
         });
 
-        Self { state, dispatcher }
+        Self {
+            state,
+            dispatcher,
+            subscribers,
+            next_subscriber_id,
+        }
     }
 
     pub fn dispatch(&self, action: Action) {
@@ -50,5 +75,20 @@ where
 
     pub fn get_state(&self) -> State {
         self.state.read().clone()
+    }
+
+    pub fn subscribe<F>(&self, callback: F) -> impl Fn() + '_
+    where
+        F: Fn(&State) + Send + Sync + 'static,
+    {
+        let subscriber_id = self.next_subscriber_id.fetch_add(1, Ordering::Relaxed);
+        let mut subscriber_map = self.subscribers.write();
+        subscriber_map.insert(subscriber_id, Box::new(callback));
+        move || self.unsubscribe(subscriber_id)
+    }
+
+    fn unsubscribe(&self, subscriber_id: usize) {
+        let mut subscriber_map = self.subscribers.write();
+        subscriber_map.remove(&subscriber_id);
     }
 }
